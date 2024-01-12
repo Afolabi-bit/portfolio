67f652c1638c763a8e2205e4432f16fa009810c7
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2024-01-12 12:34:16 +0100

    first commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="w-screen h-screen bg-slate-500 flex place-items-center justify-center">
+      <h1 className="text-white">Hello World</h1>
+    </main>
+  );
+};
+
+export default Home;
