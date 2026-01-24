044bfe6401a2d12795f929c41895838b97ec1abe
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2026-01-24 19:20:11 +0100

    feat: add `FeaturedProjectCard` component to display project details with links and tech stack.

--- src/components/projects/FeaturedProjectCard.jsx
+++ src/components/projects/FeaturedProjectCard.jsx
@@ -4,7 +4,7 @@ import { TechStack } from "@/components/ui/tech-icon";
 
 const FeaturedProjectCard = ({ data }) => {
   return (
-    <div className="big-card relative bg-transparent border border-[var(--dark-text-alt)] rounded-xl h-[270px] px-[40px] pt-[40px] pb-[50px] flex flex-col justify-between transition-all 4xl:h-[280px] 4xl:p-[60px]">
+    <div className="big-card relative bg-transparent border border-[var(--dark-text-alt)] rounded-xl h-[250px] px-[40px] pt-[40px] pb-[50px] flex flex-col justify-between transition-all 4xl:h-[280px] 4xl:p-[60px]">
       <GlowingEffect
         spread={50}
         glow={true}
@@ -18,7 +18,7 @@ const FeaturedProjectCard = ({ data }) => {
         <h2 className="text-[20px] text-white font-[700] 4xl:text-[30px] 4xl:mb-[10px]">
           {data.name}
         </h2>
-        <div className="my-[8px]">
+        <div className="my-[15px]">
           <TechStack
             stack={data.stack}
             size="w-5 h-5 4xl:w-6 4xl:h-6"
